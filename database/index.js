d41381acfe8576f1ba550b71a01f16768f5c2b60
elchangtw <elchang.tw@gmail.com>
2020-01-12 22:46:30 -0800

    adds grayscale color options

--- database/index.js
+++ database/index.js
@@ -1,31 +1,24 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-const db_url = 'mongodb://localhost:27017/';
+const db_url = 'mongodb://localhost:27017';
+const db_name = 'pixelcanvas';
 
+const client = new MongoClient(db_url);
 
-
-var pixelData = {
-  R_channel: 50,
-  G_channel: 50,
-  B_channel: 50,
-  lastEditedBy: 'Example',
-  lastEditedAt: new Timestamp(),
-};
-
-
-
-
-
-
-
-
-
-
-MongoClient.connect(db_url, function (err, client) {
+client.connect( (err) => {
   assert.equal(null, err);
-
-
-
-  // client.close();
+  console.log("Connected successfully to MongoDB.");
+  const db = client.db(db_name);
+  client.close();
 });
+
+module.exports = client;
+
+// var pixelData = {
+//   R_channel: 50,
+//   G_channel: 50,
+//   B_channel: 50,
+//   lastEditedBy: 'Example',
+//   lastEditedAt: new Timestamp(),
+// };
--- database/seed.js
+++ database/seed.js
@@ -3,6 +3,7 @@ const assert = require('assert');
 
 const db_url = 'mongodb://localhost:27017';
 const db_name = 'pixelcanvas';
+const collection_name = 'canvas'
 
 const client = new MongoClient(db_url);
 
@@ -10,5 +11,23 @@ client.connect( (err) => {
   assert.equal(null, err);
   console.log("Connected successfully to MongoDB.");
   const db = client.db(db_name);
+  const collection = db.collection(collection_name);
+
+  var pixelData = {
+    rowNum: 0,
+    colNum: 0,
+    RGBA_channels: [250, 250, 250, 255],
+    // R_channel: 250,
+    // G_channel: 250,
+    // B_channel: 250,
+    // A_channel: 255,
+    lastEditedBy: 'admin',
+    lastEditedAt: new Date(),
+  };
+  
+  db.collection(collection_name).updateOne({rowNum: pixelData.rowNum, colNum: pixelData.colNum}, pixelData, {upsert: true}, function(err, result) {
+    // db.close();
+  });
+
   client.close();
 });
